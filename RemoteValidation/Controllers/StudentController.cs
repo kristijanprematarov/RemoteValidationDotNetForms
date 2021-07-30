ea947c271b84726de4771a60cdf72310214b743f
Kristijan Prematarov <kristijanprematarov@yahoo.com>
2021-07-30 19:17:42 +0200

    final

---
+++ RemoteValidation/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using RemoteValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RemoteValidation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            StudentModel sm = new StudentModel();
+            return View(sm);
+        }
+
+        [HttpGet]
+        public JsonResult IsEmailExist(string Email)
+        {
+            bool isExist = false;
+            if (Email.Equals("abc@gmail.com"))
+            {
+                isExist = true;
+            }
+            return Json(!isExist, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ RemoteValidation/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace RemoteValidation.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        [Remote("IsEmailExist", "Student", ErrorMessage = "Email Already Exist. Please choose another email.")]
+        public string Email { get; set; }
+    }
+}
